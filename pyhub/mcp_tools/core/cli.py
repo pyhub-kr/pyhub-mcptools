01cc55eb73832608ebc93f193839d55ffd5f7f01
Chinseok Lee <me@pyhub.kr>
2025-04-05 21:20:07 +0900

    mcp tool 호출 명령 call 추가

--- pyhub/mcp_tools/core/cli.py
+++ pyhub/mcp_tools/core/cli.py
@@ -1,8 +1,14 @@
+import json
+from typing import Optional, Sequence
+
 import typer
 from asgiref.sync import async_to_sync
-from pydantic import BaseModel
+from mcp.types import TextContent, ImageContent, EmbeddedResource
+from mypy.server.update import is_verbose
+from pydantic import BaseModel, ValidationError
 from rich.console import Console
 from rich.table import Table
+from typer import Argument
 
 from .choices import TransportChoices
 from .init import mcp
@@ -26,8 +32,59 @@ def run(
     mcp.run(transport=transport)
 
 
+@app.command()
+def call(
+    tool_name: str = Argument(..., help="tool name"),
+    tool_args: Optional[list[str]] = Argument(
+        None,
+        help="Arguments for the tool in key=value format(e.g, x=10 y='hello world'",
+    ),
+):
+    """테스트 목적으로 MCP 인터페이스를 거치지 않고 지정 도구를 직접 호출"""
+
+    arguments = {}
+    if tool_args:
+        for arg in tool_args:
+            try:
+                key, value = arg.split("=", 1)
+            except ValueError:
+                console.print(f"[red]Invalid argument format: '{arg}'. Use key=value[/red]")
+                raise typer.Exit(1)
+
+            # Attempt to parse value as JSON
+            try:
+                arguments[key] = json.loads(value)
+            except json.JSONDecodeError:
+                # Fallback to string if not valid JSON
+                arguments[key] = value
+
+    console.print(f"Calling tool '{tool_name}' with arguments: {arguments}")
+
+    return_value: Sequence[TextContent | ImageContent | EmbeddedResource]
+    try:
+        return_value = async_to_sync(mcp.call_tool)(tool_name, arguments=arguments)
+    except ValidationError as e:
+        console.print(f"[red]{e}[/red]")
+        raise typer.Exit(1)
+    except Exception as e:
+        console.print(f"[red]{e}[/red]")
+        raise typer.Exit(1)
+    else:
+        for ele in return_value:
+            if isinstance(ele, TextContent):
+                console.print(ele.text)
+            elif isinstance(ele, ImageContent):
+                console.print(ele)
+            elif isinstance(ele, EmbeddedResource):
+                console.print(ele)
+            else:
+                raise ValueError(f"Unexpected type : {type(ele)}")
+
+
 @app.command(name="list")
 def list_():
+    """tools/resources/resource_templates/prompts 목록 출력"""
+
     tools = async_to_sync(mcp.list_tools)()
     resources = async_to_sync(mcp.list_resources)()
     resource_templates = async_to_sync(mcp.list_resource_templates)()
