0a8323f42136de6b7817339bdc7f9aca7ee0524d
Chinseok Lee <me@pyhub.kr>
2025-04-08 15:04:50 +0900

    pyhub.mcptools 명령에 --version 인자 추가

--- pyhub/mcptools/__main__.py
+++ pyhub/mcptools/__main__.py
@@ -1,4 +1,5 @@
 from importlib import import_module
+from importlib.metadata import version, PackageNotFoundError
 
 import typer
 
@@ -26,13 +27,23 @@ logo = """
 
 
 @app.callback(invoke_without_command=True)
-def main(ctx: typer.Context):
-    # 사용할 Tools 들을 임포트
-    import_module("pyhub.mcptools.excel.tools")
+def main(
+    ctx: typer.Context,
+    is_version: bool = typer.Option(False, "--version", "-v", help="Show version and exit."),
+):
+    if is_version:
+        try:
+            v = version("pyhub-mcptools")
+        except PackageNotFoundError:
+            v = "not found"
+        console.print(v, highlight=False)
+    else:
+        # 사용할 Tools 들을 임포트
+        import_module("pyhub.mcptools.excel.tools")
 
-    if ctx.invoked_subcommand is None:
-        console.print(logo)
-        console.print(ctx.get_help())
+        if ctx.invoked_subcommand is None:
+            console.print(logo)
+            console.print(ctx.get_help())
 
 
 if __name__ == "__main__":
