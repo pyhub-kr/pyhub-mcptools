40e64cfd144a508efec5ec07db9fcc9ff903bd8b
Chinseok Lee <me@pyhub.kr>
2025-04-19 23:56:57 +0900

    테스트 기본 language code 설정을 en-US로 강제

--- pyhub/mcptools/conftest.py
+++ pyhub/mcptools/conftest.py
@@ -1,6 +1,15 @@
 """Pytest configuration file."""
 
 import pytest
+from django.utils.translation import activate, deactivate
+
+
+@pytest.fixture(autouse=True)
+def use_english_language():
+    """Set English as the default language for all tests."""
+    activate('en-US')
+    yield
+    deactivate()
 
 
 @pytest.fixture
